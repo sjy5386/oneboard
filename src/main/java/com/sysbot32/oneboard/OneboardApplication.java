b86d1ce129b7195c98b9a8a9ec1c35c8a32bc479
Seokjin Yoon <sjy5386@gmail.com>
2022-05-27 17:58:16 +0900

    First commit

---
+++ src/main/java/com/sysbot32/oneboard/OneboardApplication.java
@@ -0,0 +1,13 @@
+package com.sysbot32.oneboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sysbot32/oneboard/OneboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysbot32.oneboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
