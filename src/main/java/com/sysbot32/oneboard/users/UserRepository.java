7ae96ab62d2160fbeadf00b350052431b1dc182d
Seokjin Yoon <sjy5386@gmail.com>
2022-05-27 19:56:08 +0900

    Add UserRepository

---
+++ src/main/java/com/sysbot32/oneboard/users/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sysbot32.oneboard.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
