bb1f99ad20e17bd211c625721371d4ebf277ab64
Seokjin Yoon <sjy5386@gmail.com>
2022-05-27 20:57:18 +0900

    Add ProductCategoryRepository

---
+++ src/main/java/com/sysbot32/oneboard/shops/products/ProductCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.sysbot32.oneboard.shops.products;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+}
