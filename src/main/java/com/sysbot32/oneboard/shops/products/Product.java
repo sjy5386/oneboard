5ca15b04daeb7218d9d519d9254d4a8cfe64d751
Seokjin Yoon <sjy5386@gmail.com>
2022-05-27 20:14:03 +0900

    Add Product

---
+++ src/main/java/com/sysbot32/oneboard/shops/products/Product.java
@@ -0,0 +1,54 @@
+package com.sysbot32.oneboard.shops.products;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    /* 내부 상품명 */
+    @Column(nullable = false)
+    private String name;
+
+    /* 상품명 */
+    @Column(nullable = false)
+    private String displayName;
+
+    /* 가격 */
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    /* 할인가 */
+    private BigDecimal discountedPrice;
+
+    /* 설명 */
+    private String description;
+
+    /* 숨김 */
+    @Column(nullable = false)
+    private Boolean isHidden = false;
+
+    /* 품절 */
+    @Column(nullable = false)
+    private Boolean isSoldOut = false;
+}
