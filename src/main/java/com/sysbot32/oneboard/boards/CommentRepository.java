05a8a9f551169911a3909ac378af67ad1609e732
Seokjin Yoon <sjy5386@gmail.com>
2022-05-27 20:02:23 +0900

    Add CommentRepository

---
+++ src/main/java/com/sysbot32/oneboard/boards/CommentRepository.java
@@ -0,0 +1,14 @@
+package com.sysbot32.oneboard.boards;
+
+import com.sysbot32.oneboard.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByPost(Post post);
+
+    List<Comment> findAllByUser(User user);
+}
