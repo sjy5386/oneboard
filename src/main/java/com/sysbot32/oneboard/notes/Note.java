ebb2134254aa2de552e10810e2e378e08a1da75e
Seokjin Yoon <sjy5386@gmail.com>
2022-05-30 18:15:23 +0900

    Add Note

---
+++ src/main/java/com/sysbot32/oneboard/notes/Note.java
@@ -0,0 +1,40 @@
+package com.sysbot32.oneboard.notes;
+
+import com.sysbot32.oneboard.users.User;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    private User from;
+
+    @ManyToOne
+    private User to;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Boolean read = false;
+}
