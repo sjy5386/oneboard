95c24885e4a7dd7fc5a9e41b36afe6b9dbf90a21
Seokjin Yoon <sjy5386@gmail.com>
2022-05-30 20:10:36 +0900

    Add PostCategoryRepository

---
+++ src/main/java/com/sysbot32/oneboard/boards/PostCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.sysbot32.oneboard.boards;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostCategoryRepository extends JpaRepository<PostCategory, Long> {
+    List<PostCategory> findAllByBoard(Board board);
+}
